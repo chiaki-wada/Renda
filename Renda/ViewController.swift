9dc5ff87b175e7f1512d72aec826318097f225e6
Chiaki Wada <zpd2da.zipadeeay@gmail.com>
2023-05-05 22:22:45 +0900

    Initial Commit

---
+++ Renda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Renda
+//
+//  Created by Chiaki Wada on 2023/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
